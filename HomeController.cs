ac43f91c87a46d163946f7bc75a1762b17cc4e3e
nikhilnimapinfo <166018854+nikhilnimapinfo@users.noreply.github.com>
2024-06-19 19:06:06 +0530

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Filters_Project2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            try { throw new Exception("It a Exception"); }
+            catch (Exception ) {
+                return RedirectToAction("Index", "Error"); 
+            }
+            
+
+        }
+    }
+}
